c0d63cdfe2f5dc4846ff6098fad105b28e3f9322
reiven <c.t.reibun@gmail.com>
2024-04-02 18:51:37 +0900

    add nvim

---
+++ nvim/lua/community.lua
@@ -0,0 +1,21 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
+}
