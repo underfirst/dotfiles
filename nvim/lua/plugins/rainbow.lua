b2aaad8f07988f69c3d12bcc72cf4431b85b66d1
reiven <c.t.reibun@gmail.com>
2024-11-05 22:46:12 +0900

    use lazynvim

---
+++ nvim/lua/plugins/rainbow.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "hiphish/rainbow-delimiters.nvim",
+    opt = {
+      query = {
+        [""] = "rainbow-delimiters",
+        lua = "rainbow-blocks",
+      },
+      priority = {
+        [""] = 110,
+        lua = 210,
+      },
+      highlight = {
+        "RainbowDelimiterRed",
+        "RainbowDelimiterYellow",
+        "RainbowDelimiterBlue",
+        "RainbowDelimiterOrange",
+        "RainbowDelimiterGreen",
+        "RainbowDelimiterViolet",
+        "RainbowDelimiterCyan",
+      },
+    },
+  },
+}
