bbd67de79a99a9b81ab09ab88a66a078a35a2ffc
reiven <c.t.reibun@gmail.com>
2025-02-25 09:44:42 +0900

    fix: remove format_on_save

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,11 +7,11 @@ return {
         python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
         -- You can customize some of the format options for the filetype (:help conform.format)
       },
-      format_on_save = {
-        timeout_ms = 3000,
-        -- fallbackはしなくてもいい(ruff_fixが優先される)
-        lsp_format = "fallback",
-      },
+      -- format_on_save = {
+      -- timeout_ms = 3000,
+      -- fallbackはしなくてもいい(ruff_fixが優先される)
+      -- lsp_format = "fallback",
+      -- },
     },
   },
 }
