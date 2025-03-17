dc7f2d5cb5bd24468de4415941c842899d8aa08c
reiven <c.t.reibun@gmail.com>
2025-03-18 08:10:47 +0900

    add mas

---
+++ mas.sh
@@ -0,0 +1,6 @@
+mas install 409203825 # numbers
+mas install 409183694 # keynotes
+mas install 409201541 # pages
+mas install 539883307 # LINE
+mas install 408981434 # iMovie
+mas install 490505997 # skitch
