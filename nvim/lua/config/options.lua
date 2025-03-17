189cf62824ae57964d615938e4439e9cee1ae463
reiven <c.t.reibun@gmail.com>
2025-03-18 08:09:04 +0900

    add option

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.o.guifont = "Hack Nerd Font"
+
+-- show invisibles
+vim.opt.listchars = { tab = "  ", trail = "·", extends = "»", precedes = "«", nbsp = "░" }
+vim.opt.list = true
+
+-- default line wrapping
+vim.opt.wrap = true
