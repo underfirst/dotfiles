f2e4a393c07d8a05d56be64d4e2ac898dbb95794
reiven <c.t.reibun@gmail.com>
2025-03-28 08:38:55 +0900

    update tmux

---
+++ nvim/lua/plugins/glance.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "dnlhc/glance.nvim",
+    cmd = "Glance",
+    keys = {
+      { "gld", "<CMD>Glance definitions<CR>", mode = { "n" }, desc = "glance definitions." },
+      { "glr", "<CMD>Glance references<CR>", mode = { "n" }, desc = "glance references." },
+      { "glt", "<CMD>Glance type_definitions<CR>", mode = { "n" }, desc = "glance type_definitions." },
+      { "gli", "<CMD>Glance implementations<CR>", mode = { "n" }, desc = "glance implementations" },
+    },
+  },
+}
---
+++ nvim/lua/plugins/markview.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+    -- For blink.cmp's completion
+    -- source
+    -- dependencies = {
+    --     "saghen/blink.cmp"
+    -- },
+  },
+}
